1aa8b7f0164f505c313ef0d465a353bf6539cb12
rsevial <rebekahsevial@gmail.com>
2023-04-22 02:12:48 +0800

    Create a Pseudocode

---
+++ Assign 2.3-Vigenère Cipher.py
@@ -0,0 +1,26 @@
+# Programmed by: Rebekah Joy E. Sevial
+# Problem 3: Vigenère Cipher
+
+# Implementing keypadding method
+# Variable to store padded key
+# Check the chr if alpha
+        # Else, ignore and append as empty
+    # Return padded_key          
+
+# Implement encrypt character method
+# Check if the message is alpha and is an uppercase letter
+        # Define as "a"
+            # Define as "A"
+
+# Find the position of the message's char in alphabet
+# Find the  position of the key's char in alphabet
+# Find the new postion of the message 
+        # Return char
+    # If not, return message_char
+
+# Implement encrypt method
+# Ask the user to input a message and key to encrypt
+# Import module
+# Format massage and key
+# Print the output with color
+
